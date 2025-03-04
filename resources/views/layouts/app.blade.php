08d177eb0951c453d4d303521a49eda67984d5c1
JooJ15 <137832108+JooJ15@users.noreply.github.com>
2025-03-04 16:40:31 -0300

    V0.73 Perfil

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>QualityPlus+</title>
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/perfil.css') }}">
 </head>
 <body>
     @yield('content')
